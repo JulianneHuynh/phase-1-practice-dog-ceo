f3e093de47c8581542e26465bb416582d38798d9
Julianne Huynh <JulianneHuynh@yahoo.com>
2022-11-04 08:55:14 -0500

    commit tree 0f32050452a3739a3e88d2cb9f3d12beab85327d

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+
+
+function loadImages(){
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+  .then (resp => resp.json())
+  .then (results =>{
+    results.message.forEach(image =>{
+      // add image 
+      addImage(image)
+    });
+  })
+}
+function addImage(dogPicUrl) {
+  const container = document.getElementById('dog-image-container')
+  const newImage = document.createElement('img')
+  newImage.src = dogPicUrl
+  container.appendChild(newImage)
+}
+document.addEventListener('DOMContentLoaded', function (){
+  //load image 
+  loadImages()
+})
